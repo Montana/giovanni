dd87199cee9198a524beb70859adc705e1b697d8
Montana Mendy <montanamendy@gmail.com>
2016-10-17 17:05:30 -0700

    Add files via upload

---
+++ csv.rb
@@ -0,0 +1,17 @@
+
+def make_spotify_csv(songs)
+  playlist_name = songs.first.playlist_source
+  csv_filename = "#{playlist_name}-" + Time.now.strftime("%B-%d-%Y") + ".csv"
+  CSV.open("#{csv_filename}", 'wb') do |row|
+    row << SPOTIFY_PLAYLIST_HEADERS
+    songs.each do |item|
+      song_attrs = [
+        item.position, item.artist_name, item.track_name, item.facebook_likes, item.spotify_popularity,
+        item.label_info, item.general_manager, item.fb_profile_name, item.artist_web_links, item.spotify_link,
+        item.fb_genre, item.track_tempo, item.spotify_track_dancibility, item.spotify_track_instrumentalness,
+        item.spotify_track_positiveness, item.chart_source, item.time_of_scraping, item.loudness
+        ]
+      row << song_attrs
+    end
+  end
+end
---
+++ giovanni.rb
@@ -0,0 +1,49 @@
+RSpotify.authenticate(ENV['SPOTIFY_TOKEN'], ENV['SPOTIFY_SECRET'])
+
+def spotify_user_playlist_search(user)
+  puts "Scraping data from Spotify..."
+  latest_playlist = user.playlists.first
+  songs = []
+
+  latest_playlist.tracks.each_with_index do |track, index|
+    song = Song.new
+    song.playlist_source = latest_playlist.name
+    song.track_name = track.name
+    song.spotify_track_id = track.id
+    song.artist_name = track.artists.first.name
+    song.position = index + 1
+    song.time_of_scraping = Time.now.strftime("%A %B/%d/%Y")
+    songs << song
+  end
+
+  songs.each do |song|
+    spotify_artist = RSpotify::Artist.search(song.artist_name)
+    song.spotify_popularity = spotify_artist.last.popularity
+    song.spotify_link = spotify_artist.last.external_urls['spotify']
+  end
+  songs
+end
+
+def spotify_audiofeatures_search(songs)
+  songs.each do |song|
+    audio_features = RSpotify::AudioFeatures.find(song.spotify_track_id)
+    song.track_tempo = audio_features.tempo.to_s
+    song.spotify_track_dancibility = audio_features.danceability.to_s
+    song.spotify_track_instrumentalness = audio_features.instrumentalness.to_s
+    song.spotify_track_positiveness = audio_features.valence.to_s
+    song.spotify_track_loudness = audio_feautres.loudness.to_s
+  end
+  songs
+end
+
+def spotify_artist_search(songs)
+  puts "Scraping data from Spotify..."
+  songs.each do |song|
+    spotify_artist = RSpotify::Artist.search(song.artist_name)
+    if spotify_artist.any?
+      song.spotify_popularity = spotify_artist.last.popularity
+      song.spotify_link = spotify_artist.last.external_urls['spotify']
+    end
+  end
+  songs
+end
